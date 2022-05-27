1efc23d6748a5a4657443501519aa43e2491df1f
mort <morteza.eivazi.1380@gmail.com>
2022-05-27 16:18:38 +0430

    update on FastAPI

--- app/main.py
+++ app/main.py
@@ -13,9 +13,34 @@ async def test():
     return {"text":"Hello world"}
 
 
-
+@app.post('/user/register')
 async def Register():
     pass
 
+@app.post('/user/login')
 async def Login():
     pass
+
+@app.get('/user/follow')
+async def follow():
+    pass
+
+@app.put('/user/update')
+async def update():
+    pass
+
+@app.get('/user/retrieve')
+async def retrieve():
+    pass
+
+@app.post('/single_post/post')
+async def post():
+    pass
+
+@app.delete('/single_post/delete')
+async def delete():
+    pass
+
+
+
+
